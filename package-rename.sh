4e7559d1a01deb849896f987fa5b2c13f3b40377
Scott Greenwald <swgreen@mit.edu>
2014-04-19 14:55:16 -0400

    include keystore in archive

--- package-rename.sh
+++ package-rename.sh
@@ -83,7 +83,9 @@ echo `ls -l $pathToNewApk`
 
 # debug.keystore is file, assumed to be in current directory
 export newName=$truncatedName-renamed.apk
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore debug.keystore $pathToNewApk android
+export SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $SCRIPTPATH/debug.keystore $pathToNewApk android
 cp $pathToNewApk $newName
 
 #
